eceff13bbdb56ac4a577cf60ded24601314e5cb0
csae8092 <peter.andorfer@oeaw.ac.at>
2022-01-30 13:37:44 +0100

    download bs

---
+++ setup.sh
@@ -0,0 +1,7 @@
+# bin/bash
+BOOTSTRAP_VERSION=5.0.2
+BOOTSTRAP=bootstrap-${BOOTSTRAP_VERSION}-examples
+mkdir ./html
+rm -rf ${BOOTSTRAP} && rm -rf ./html/assets
+wget https://github.com/twbs/bootstrap/releases/download/v${BOOTSTRAP_VERSION}/${BOOTSTRAP}.zip && unzip ${BOOTSTRAP}.zip && rm -rf ${BOOTSTRAP}.zip
+mv ./${BOOTSTRAP}/assets ./html/assets && rm -rf ${BOOTSTRAP}
